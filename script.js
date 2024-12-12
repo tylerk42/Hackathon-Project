2e23d9a4db50c23c7256066f572faa17b60577ce
GooseoftheGeese <anorambuena28@students.stab.org>
2024-12-12 11:56:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var url="https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Geography/US%20Voter%20Registration%20and%20Demographics%20By%20State.csv"
+
+
+function showSpeed(){
+    document.getElementById("sliderVal").innerHTML = document.getElementById("coasterSpeed").value
+}
+function showHeight(){
+    document.getElementById("sliderValue").innerHTML = document.getElementById("coasterHeight").value
+}
+function showLength(){
+    document.getElementById("sliderNum").innerHTML = document.getElementById("coasterLength").value
+}
+
