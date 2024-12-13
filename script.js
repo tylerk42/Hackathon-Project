4f7e942002d2ab0003aeb1ad1e869c71e92e7daa
tylerk42 <tkerrigan28@students.stab.org>
2024-12-13 13:06:21 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function showLength(){
     document.getElementById("sliderNum").innerHTML = document.getElementById("coasterLength").value
 }
 
+
 var coasterName= getColumn(url,1)
 var parkName= getColumn(url,2)
 var city= getColumn(url,3)
@@ -36,35 +37,7 @@ function createlist(material, region, speed, height, length){
     var heightWork=false
     var lengthWork=false
     for(var i=0; i<coasterName.length; i++){
-        if (rollerSpeed[i]<=50&&speed<=50){
-            speedWork=true
-        }
-        else if (rollerSpeed[i]<=100&&rollerSpeed[i]>50&&speed<=100&&speed>50){
-            speedWork=true
-        }
-        else if (rollerSpeed[i]>100&&speed>100){
-            speedWork=true
-            console.log("w")
-        }
-        else if (rollerHeight[i]<=15&&height<=15){
-            heightWork=true
-        }
-        else if (rollerHeight[i]<=75&&height[i]>15&&height<=75&&height>15){
-            heightWork=true
-        }
-        else if (rollerHeight[i]>75&&speed>75){
-            heightWork=true
-        }
-        else if (rollerLength[i]<=250&&length<=250){
-            lengthWork=true
-        }
-        else if (rollerLength[i]<=1000&&length[i]>250&&length<=1000&&length>250){
-            lengthWork=true
-        }
-        else if (rollerLength[i]>1000&&length>1000){
-            lengthWork=true
-        }
-        if(material==constructionMaterial[i]&&region==parkRegion[i]&&lengthWork==true&&heightWork==true&&speedWork==true){
+        if(material==constructionMaterial[i]&&region==parkRegion[i]&&Math.abs(rollerSpeed[i] - speed) < 10&&Math.abs(rollerHeight[i] - height) < 10&&Math.abs(rollerLength[i] - length) < 150){
             infoList= "(Coaster Name: "+coasterName[i]+", Park Name:"+parkName[i]+", City:"+city[i]+", Country: "+country[i]+")"
             matchingOutputs.push(infoList)
             // console.log(infoList)
