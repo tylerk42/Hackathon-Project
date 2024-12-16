160cc21ac033566a61da809cd1d0998b84dcf051
GooseoftheGeese <anorambuena28@students.stab.org>
2024-12-16 13:20:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,28 +16,35 @@ var city= getColumn(url,3)
 var country= getColumn(url,4)
 var parkRegion= getColumn(url,5)
 var constructionMaterial= getColumn(url,6)
-var rollerHeight= getColumn(url,7)
-var rollerSpeed= getColumn(url,8)
-var rollerLength= getColumn(url,9)
+var rollerHeight= getColumn(url,7)*3.28
+var rollerSpeed= getColumn(url,8)*.62
+var rollerLength= getColumn(url,9)*3.28
+var inversion=getColumn(url,10)
 
 // function createlist(){
 //     console.log(document.getElementById("coasterHeight").value)
 // }
-
+var inversionStatus="no";
+function inversionTrue(){
+inversionStatus="yes"
+}
+function inversionFalse(){
+    inversionStatus="no"
+}
 function createlist(material, region, speed, height, length){
     document.getElementById("output").innerHTML=""
     var matchingOutputs=[];
     var infoList;
     var material = document.getElementById("material").value;
-    var region= document.getElementById("region").value;
-    var speed= document.getElementById("coasterSpeed").value;
-    var height= document.getElementById("coasterHeight").value;
-    var length= document.getElementById("coasterLength").value;
-    var speedWork=false
-    var heightWork=false
-    var lengthWork=false
+    var region = document.getElementById("region").value;
+    var speed = document.getElementById("coasterSpeed").value;
+    var height = document.getElementById("coasterHeight").value;
+    var length = document.getElementById("coasterLength").value;
+    var speedWork =false
+    var heightWork =false
+    var lengthWork =false
     for(var i=0; i<coasterName.length; i++){
-        if(material==constructionMaterial[i]&&region==parkRegion[i]&&Math.abs(rollerSpeed[i] - speed) < 30&&Math.abs(rollerHeight[i] - height) < 20&&Math.abs(rollerLength[i] - length) < 500){
+        if(material==constructionMaterial[i]&&region==parkRegion[i]&&Math.abs(rollerSpeed[i] - speed) < 20&&Math.abs(rollerHeight[i] - height) < 20&&Math.abs(rollerLength[i] - length) < 500&&inversion[i]==inversionStatus){
             infoList= "(Coaster Name: "+coasterName[i]+", Park Name:"+parkName[i]+", City:"+city[i]+", Country: "+country[i]+")"
             matchingOutputs.push(infoList)
             // console.log(infoList)
@@ -52,5 +59,6 @@ function createlist(material, region, speed, height, length){
     speedWork=false
     heightWork=false
     lengthWork=false
+    inversionStatus="no"
 
 }
